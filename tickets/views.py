05c945f7b1c120b2891a6bd270fe92e99ec74bcf
Kalugin68 <sashakalugin74@gmail.com>
2026-05-26 14:35:49 +0300

    feat(ticket): Change GET request to retrieve user role data

--- tickets/views.py
+++ tickets/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from users.models import User
 from .models import Ticket
 from .serializers import TicketSerializer
 
 
 class TicketViewSet(ModelViewSet):
-    queryset = Ticket.objects.all()
-
     serializer_class = TicketSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.role in (
+            User.Role.ADMIN,
+            User.Role.SUPPORT,
+        ):
+            return Ticket.objects.all()
+        else:
+            return Ticket.objects.filter(author=user)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
